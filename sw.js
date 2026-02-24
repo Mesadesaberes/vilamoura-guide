8b23102ba6d010db82360e75a49b8bc307e8e7aa
Mesadesaberes <mesadesaberes@gmail.com>
2026-02-24 20:47:35 +0000

    Atualizar com 10 categorias

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'vilamoura-select-v12';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativar Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta pedidos de rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
